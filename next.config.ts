5a222da7241dcf8b993110e390e65e262ce9f0d4
helpbirbal-cmyk <helpbirbal@gmail.com>
2025-09-01 15:18:16 +0530

    fixed nextconfig fixs commit

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,18 @@
 const nextConfig = {
   reactStrictMode: false,
   async headers() {
-   return [
-     {
-       source: '/(.*)',
-       headers: [
-         {
-           key: 'Content-Security-Policy',
-           value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; img-src 'self' data: https://*.tile.openstreetmap.org; font-src 'self' https://fonts.gstatic.com; connect-src 'self' *.supabase.co wss://*.supabase.co;",
-         },
-       ],
-     },
-   ];
- },
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; img-src 'self' data: https://*.tile.openstreetmap.org; font-src 'self' https://fonts.gstatic.com; connect-src 'self' *.supabase.co wss://*.supabase.co;",
+          },
+        ],
+      },
+    ];
+  },
   experimental: {
     turbo: {
       // Enable Turbopack for faster builds
